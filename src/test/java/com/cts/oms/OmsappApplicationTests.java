eb73ff69c77ab829faea40d8f0f66fb1a9792d79
unknown <rkrishan.thakur61@gmail.com>
2022-11-12 15:28:29 +0530

    proj setup

---
+++ src/test/java/com/cts/oms/OmsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
